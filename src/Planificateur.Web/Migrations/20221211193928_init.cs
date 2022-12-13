bbe2cc6c0ceccd5f177b61716ff55b9a681756af
Ombrelin <arsene@lapostolet.fr>
2022-12-13 19:39:15 +0100

    commit db migrations

---
+++ src/Planificateur.Web/Migrations/20221211193928_init.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planificateur.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Dates = table.Column<List<DateTime>>(type: "timestamp with time zone[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Votes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PollId = table.Column<Guid>(type: "uuid", nullable: false),
+                    VoterName = table.Column<string>(type: "text", nullable: false),
+                    Availabilities = table.Column<int[]>(type: "integer[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Votes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Votes_Polls_PollId",
+                        column: x => x.PollId,
+                        principalTable: "Polls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_PollId",
+                table: "Votes",
+                column: "PollId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Votes");
+
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}
---
+++ test/Planificateur.Web.Tests/Database/DatabaseFixture.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Planificateur.Web.Database;
+
+namespace Planificateur.Web.Tests.Database;
+
+public class DatabaseFixture : IAsyncLifetime {
+    public  ApplicationDbContext DbContext { get; }
+
+    public DatabaseFixture()
+    {
+
+        DbContext = BuildNewDbContext();
+    }
+    
+    private static ApplicationDbContext BuildNewDbContext()
+    {
+        var builder = new NpgsqlConnectionStringBuilder
+        {
+            Host = Environment.GetEnvironmentVariable("DB_HOST"),
+            Port = int.Parse(Environment.GetEnvironmentVariable("DB_PORT")),
+            Username = Environment.GetEnvironmentVariable("DB_USERNAME"),
+            Password = Environment.GetEnvironmentVariable("DB_PASSWORD"),
+            Database = Environment.GetEnvironmentVariable("DB_NAME")
+        };
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql(builder.ToString())
+            .Options;
+
+        return new ApplicationDbContext(options);
+    }
+
+    public async Task InitializeAsync() => await DbContext.Database.MigrateAsync();
+
+    public async Task DisposeAsync()
+    {
+        await DbContext.Database.EnsureDeletedAsync();
+        await DbContext.DisposeAsync();
+    }
+}
