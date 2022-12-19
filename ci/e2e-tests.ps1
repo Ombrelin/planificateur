f5b5cc7e6777d1f139ed919b8ab304e9384aabcb
Ombrelin <arsene@lapostolet.fr>
2022-12-19 16:41:16 +0100

    add failure of ci in case e2e tests fail

--- ci/e2e-tests.ps1
+++ ci/e2e-tests.ps1
@@ -21,14 +21,17 @@ docker run -d `
 
 $pwd = pwd;
 
-$result = docker run --rm `
+docker run `
     --env APP_URL="http://planificateur-e2e/" `
     --env IS_CI="true" `
     --net planificateur `
+    --name planificateur-e2e-tests-exec `
     -v "${pwd}:/app" `
     -w /app `
     ombrelin/dotnet7-playwright `
     /bin/bash -c "dotnet build /app/test/Planificateur.Web.EndToEndTests/Planificateur.Web.EndToEndTests.csproj && ~/bin/playwright install-deps && ~/bin/playwright install firefox && dotnet test /app/test/Planificateur.Web.EndToEndTests/Planificateur.Web.EndToEndTests.csproj"
+
+$result = docker inspect planificateur-e2e-tests-exec --format='{{.State.ExitCode}}'
 
 docker stop planificateur-e2e
 docker rm planificateur-e2e
