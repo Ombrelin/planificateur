28cec25de83dbae2f919618ab2f72ac1e5fda2e1
Ombrelin <arsene@lapostolet.fr>
2022-12-20 15:22:35 +0100

    fix icons + start to implement date range

--- src/Planificateur.Web/ClientScripts/script.ts
+++ src/Planificateur.Web/ClientScripts/script.ts
@@ -1,24 +1,33 @@
-const addDate = () => {
+const addDate = (date: Date | null = null) => {
     const dates = document.querySelector("#dates");
     const newDate = document.createElement("div");
     newDate.classList.add("columns","mb-0");
     newDate.innerHTML = `
         <div class="field mb-0 column is-three-quarters">
             <div class="control">
-                <input class="input" type="date" name="dates[]"/>
+                <input class="input" value="${date?.toISOString().slice(0, 16)}" type="datetime-local" name="dates[]"/>
             </div>
         </div>
         <div class="column  is-flex is-justify-content-end">
             <button type="button" onclick="removeDate(this)" class="button">
                 <span class="icon">
-                  <i class="delete"></i>
-                </span>                
+                  <i class="fas fa-trash"></i>
+                </span>
             </button>
         </div>
     `;
     dates?.insertAdjacentElement( "afterend", newDate);
 }
 
+const addDateRange = () => {
+    const startDate = new Date(document.querySelector("#range-start-date")?.getAttribute("value") ?? "");
+    const endDate = new Date(document.querySelector("#range-start-date")?.getAttribute("value") ?? "");
+    
+    for(let currentDate: Date = startDate ; currentDate <= endDate ; currentDate.setDate(currentDate.getDate() + 1)){
+        addDate(currentDate);
+    }
+}
+ 
 const removeDate = (element: HTMLElement) => {
     element.parentElement?.parentElement?.remove();
 }
