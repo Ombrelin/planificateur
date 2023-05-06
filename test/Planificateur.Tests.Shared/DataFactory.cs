0b744e743d5e3547ec963be586b2fcd1cf6f12b4
Arsène Lapostolet <arsene@lapostolet.fr>
2023-05-07 00:48:06 +0200

    add missing files

---
+++ test/Planificateur.Tests.Shared/DataFactory.cs
@@ -0,0 +1,13 @@
+﻿using Planificateur.Core.Entities;
+
+namespace Planificateur.Tests.Shared;
+
+public class DataFactory
+{
+    private static int testUsersCount = 0;
+    
+    public readonly string Username = "John Shepard";
+    public readonly string Password = "Test1";
+
+    public ApplicationUser BuildTestUser() => new ($"{Username}-{++testUsersCount}", Password);
+}
